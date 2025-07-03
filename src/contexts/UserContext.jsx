b4a6229875da0247d3099c5c903f3a5849f61de6
EmmanuelAjibokun <emmanuelajibokun9@gmail.com>
2025-07-03 15:23:16 +0100

    Refactor: update UserContext to fetch user data and wrap App with UserProvider

--- src/contexts/UserContext.jsx
+++ src/contexts/UserContext.jsx
@@ -1,25 +1,53 @@
 import { createContext, useContext, useEffect, useState } from "react";
 import { useLocation  } from "react-router-dom";
+import PropTypes from "prop-types";
 
 const UserContext = createContext();
 
-export const useUser = () => useContext(UserContext);
+export const useUser = () => {
+    const context = useContext(UserContext);
+    if (context === undefined) {
+        throw new Error('useUser must be used within a <UserProvider>');
+    }
+    return context;
+}
 
-export const UserProvider = ({ children, param }) => {
+export const UserProvider = ({ children }) => {
     const [userData, setuserData] = useState(null);
     const location = useLocation();
-
+    
     useEffect(() => {
+        // Extract the userId/username from the URL path
+        const pathParts = location.pathname.split('/');
+        // const username = pathParts[2];
+        const username = true;
+        console.log("location: ", pathParts)
+
         async function fetchData() {
-            const res = await fetch(`http://localhost:5000/api/user/${param}`);
-            const data = await res.json();
-            setuserData(data);
+            try {
+                console.log("start fetching")
+                // const res = await fetch(`http://localhost:5000/api/user/${username}`);
+                const res = await fetch('../../data.json');
+                if (!res.ok) throw new Error('Failed to fetch user');
+                const data = await res.json();
+                console.log("new data", data);
+                setuserData(data);
+            } catch (err) {
+                console.error(err);
+            }
+            
+            UserProvider.propTypes = {
+                children: PropTypes.node.isRequired,
+            };
         }
-        fetchData();
-    }, [param]);
+
+        if (username) {
+            fetchData();
+        }
+    }, [location.pathname]);
 
     return (
-        <UserContext.Provider value={{ userData, setuserData }}>
+        <UserContext.Provider value={userData}>
             {children}
         </UserContext.Provider>
     );
--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,16 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 import { Analytics } from '@vercel/analytics/react';
-import './index.css'
+import './index.css';
+import { UserProvider } from './contexts/UserContext';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
-      <Analytics />
+      <UserProvider>
+        <App />
+        <Analytics />
+      </UserProvider>
     </BrowserRouter>
   </React.StrictMode>
 )
